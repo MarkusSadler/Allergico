47e9885d25cc12af5fe7e6e5fa56ad452e840848
Markus Sadler <msadler@live.at>
2015-05-25 11:48:04 +0200

    ProductCategoryDAO contains now a List which holds all ProductsCategories From the DB.

--- app/src/main/java/at/allergico/allergico/database/DAO/ProductCategoryDAO.java
+++ app/src/main/java/at/allergico/allergico/database/DAO/ProductCategoryDAO.java
@@ -1,8 +1,17 @@
 package at.allergico.allergico.database.DAO;
 
+import android.media.Image;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import at.allergico.allergico.database.Manager.DBManager;
 import at.allergico.allergico.database.POJO.ProductCategoryPOJO;
+import at.allergico.allergico.database.POJO.ProductPOJO;
 
 /**
  * Created by Michael on 23/05/2015.
@@ -21,12 +30,38 @@ public class ProductCategoryDAO {
     }
 
     private ProductCategoryDAO() {
-
+        getAllProductCategories();
     }
     /******** SINGLETON END *********/
+    DBManager dbManager = DBManager.getInstance();
+
+    private List<ProductCategoryPOJO> _productCatList = new ArrayList<>();
+    public List<ProductCategoryPOJO> getProductCatList() {
+        return _productCatList;
+    }
 
     public List<ProductCategoryPOJO> getAllProductCategories() {
-        throw new UnsupportedOperationException();
+        this.getProductCatList().clear();
+        String jsonString = dbManager.getObject("Product");
+        try {
+            JSONArray jsonArray = new JSONArray(jsonString);
+            JSONObject[] jsonObjects = new JSONObject[jsonArray.length()];
+            for(int i = 0; i < jsonArray.length(); i++){
+                jsonObjects[i] = jsonArray.getJSONObject(i);
+            }
+            for(JSONObject item : jsonObjects){
+                ProductCategoryPOJO productCat = new ProductCategoryPOJO(
+                        item.getInt("ProductCatID"),
+                        item.getString("ProductCatName")
+
+                );
+
+                this.getProductCatList().add(productCat);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return this.getProductCatList();
     }
 
     public List<ProductCategoryPOJO> getProductCategoryByName(String categoryName) {
@@ -36,4 +71,6 @@ public class ProductCategoryDAO {
     public ProductCategoryPOJO getProductCategoryByID(int categoryID) {
         throw new UnsupportedOperationException();
     }
+
+
 }
