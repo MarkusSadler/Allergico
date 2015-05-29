0551fc96e7b87d06727f8eb56a5567ddf6edc9b0
Scrumi13 <michael.mosgoeller@gmx.net>
2015-05-29 15:49:14 +0200

    colour helper

--- app/src/main/java/at/allergico/allergico/helper/ColourHelper.java
+++ app/src/main/java/at/allergico/allergico/helper/ColourHelper.java
@@ -7,9 +7,14 @@ import android.graphics.Color;
  */
 public class ColourHelper {
 
+    //Button Colours
     public static final int NAVI_BUTTON_FORWARD = Color.parseColor("#90EE90");
     public static final int NAVI_BUTTON_BACK = Color.parseColor("#EEE8AA");
     public static final int ACTION_BUTTON_RESET = Color.parseColor("#F08080");
     public static final int ACTION_BUTTON_DO = Color.parseColor("#B0C4DE");
+
+    //List Colours
+    public static final int SELECTED_LIST_ELEMENT = Color.parseColor("#90EE90");
+
 
 }
