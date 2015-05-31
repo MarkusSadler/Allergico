9ad666302be76d81fc1c1c82bfd314d30503a732
Scrumi13 <michael.mosgoeller@gmx.net>
2015-05-31 22:26:15 +0200

    add administrate allergen

--- app/src/main/java/at/allergico/allergico/activities/AdministrateAllergenActivity.java
+++ app/src/main/java/at/allergico/allergico/activities/AdministrateAllergenActivity.java
@@ -5,15 +5,39 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 import at.allergico.allergico.R;
+import at.allergico.allergico.adapters.AdministrateAllergenListViewAdapter;
+import at.allergico.allergico.database.DAO.AllergenDAO;
 
 public class AdministrateAllergenActivity extends Activity {
+
+    private ListView _lv;
+    private Button _naviButton;
+
+    private String _naviIntent = null;
+
+    private AdministrateAllergenActivityListener _listener;
+    private AdministrateAllergenListViewAdapter _listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrate_allergen);
+        this._listener = new AdministrateAllergenActivityListener();
+        this._listAdapter = new AdministrateAllergenListViewAdapter(getApplicationContext(), AllergenDAO.getInstance().getAllergeneList());
+
+        this._lv = (ListView) this.findViewById(R.id.allergeneListview);
+        this._lv.setOnItemClickListener(this._listener);
+        this._lv.setOnItemLongClickListener(this._listener);
+
+
+        this._naviButton = (Button) this.findViewById(R.id.navigationButton);
+        this._naviButton.setOnClickListener(this._listener);
     }
 
     @Override
@@ -36,5 +60,23 @@ public class AdministrateAllergenActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AdministrateAllergenActivityListener implements View.OnClickListener, AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            return false;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
     }
 }
---
+++ app/src/main/java/at/allergico/allergico/adapters/AdministrateAllergenListViewAdapter.java
@@ -0,0 +1,85 @@
+package at.allergico.allergico.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import at.allergico.allergico.R;
+import at.allergico.allergico.database.POJO.AllergenPOJO;
+
+/**
+ * Created by Michael on 31/05/2015.
+ */
+public class AdministrateAllergenListViewAdapter extends ArrayAdapter<AllergenPOJO> {
+    private final Context _context;
+    private final AllergenPOJO[] _data;
+
+
+    public AdministrateAllergenListViewAdapter(Context context, List<AllergenPOJO> values) {
+        super(context, R.layout.activity_administrate_allergen_listrow, values);
+        this._context = context;
+        this._data = (AllergenPOJO[]) values.toArray();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) this._context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.activity_administrate_allergen_listrow, parent, false);
+
+        TextView allergenName = (TextView) convertView.findViewById(R.id.allergenName);
+        TextView allergenDescription = (TextView) convertView.findViewById(R.id.allergenDescription);
+        ImageView allergenImage = (ImageView) convertView.findViewById(R.id.allergenImage);
+
+        char allergenAbbreviation = this._data[position].getAbbreviation();
+        int iconID = -1;
+        allergenName.setText(this._data[position].getAbbreviation());
+        allergenDescription.setText(this._data[position].getDescription());
+
+        /*switch (allergenAbbreviation) {
+            case 'A':
+                break;
+            case 'B':
+                break;
+            case 'C':
+                break;
+            case 'D':
+                break;
+            case 'E':
+                break;
+            case 'F':
+                break;
+            case 'G':
+                break;
+            case 'H':
+                break;
+            case 'L':
+                break;
+            case 'M':
+                break;
+            case 'N':
+                break;
+            case 'O':
+                break;
+            case 'P':
+                break;
+            case 'R':
+                break;
+            default:
+                break;
+        }
+        allergenImage.setImageResource(iconID);
+*/
+        return rowView;
+    }
+}
